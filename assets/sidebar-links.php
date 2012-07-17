9565a1fefdc32ca25fd112fd8b08f0fed1e5e32e
magic_man64 <magic_man64@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-17 22:21:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rad-dropbox-uploader/tags/1.0.1@573863 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/sidebar-links.php
@@ -0,0 +1,8 @@
+<h3 class="hndle" id="about-sidebar">Resources & Information</h3>
+<div class="inside">
+<p>
+<li><a href="http://www.radmediaco.com/plugins" target="_blank">RAD WordPress Plugins</a></li>
+<li><a href="http://www.radmediaco.com/plugins/rad-dropbox-uploader/" target="_blank">Plugin Homepage</a></li>
+<li><a href="http://docs.radmediaco.com/rad-dropbox-uploader" target="_blank">Documentation</a></li>
+</p>
+</div>
