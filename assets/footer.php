73926b5c02b9b3f19b0d42b6619b5ae2e0734e69
magic_man64 <magic_man64@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-17 19:50:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rad-dropbox-uploader/tags/1.0.1@573772 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/footer.php
@@ -0,0 +1,29 @@
+<div id="about" class="postbox">
+<div class="inside">
+<p><b>Like This Plugin? Please Share!</b></p>
+<p><table border="0">
+<tr>
+<td>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=335731409799899";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://www.radmediaco.com/plugins/rad-dropbox-uploader" data-send="false" data-layout="button_count" data-show-faces="false" data-action="recommend"></div>
+</td>
+<td style="padding-top:4px;">
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.radmediaco.com/plugins/rad-dropbox-uploader" data-text="RAD Dropbox Uploader for WordPress:" data-via="RADMediaCo">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</td>
+<td style="padding-top:4px;">
+<a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.radmediaco.com%2Fplugins%2Frad-dropbox-uploader&media=http%3A%2F%2Fimages.radmediaco.com%2Fscreenshots%2Frad-dropbox-uploader-1.jpg&description=Easily%20allow%20both%20administrators%20%26%20front%20end%20users%20to%20upload%20files%20to%20specific%20folders%20on%20your%20Dropbox%20account%20using%20administration%20interface%20or%20easily%20customizable%20front-end%20shortcode!" class="pin-it-button" count-layout="horizontal"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a>
+<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>
+</td>
+</tr>
+</table></p>
+
+</div>
+</div>
