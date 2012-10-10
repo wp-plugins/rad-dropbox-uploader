ae0bec4e6722edb05843bb9bf0b524937658e11a
magic_man64 <magic_man64@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-10 00:50:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/rad-dropbox-uploader/tags/1.1.0@610210 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/header.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/style.css" type="text/css" />
+<div class="dropbox-uploader-header">
+<div class="header">RAD Dropbox Uploader</div>
+<div class="sub-header">By <a href="http://www.radmediaco.com/" target="_blank">RAD Media Company</a></div>
+</div>
