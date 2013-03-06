7e3507f08ede5c8230fec954ff7ad727eb4353a9
AaronFagan <AaronFagan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-06 21:06:21 +0000

    v1.1.1 released.
    git-svn-id: https://plugins.svn.wordpress.org/rad-dropbox-uploader/tags/1.1.1@677137 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/header.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/style.css" type="text/css" />
+<div class="dropbox-uploader-header">
+<div class="header">RAD Dropbox Uploader</div>
+<div class="sub-header">By <a href="http://www.afwebdesign.ca/" target="_blank">af Web Design</a></div>
+</div>
