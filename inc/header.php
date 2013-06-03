abc31add054083119a0f6ae573fd85b3b007f881
AaronFagan <AaronFagan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-03 21:29:56 +0000

    v1.1.3 correction
    git-svn-id: https://plugins.svn.wordpress.org/rad-dropbox-uploader/tags/1.1.3@722289 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/header.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/style.css" type="text/css" />
+<div class="dropbox-uploader-header">
+<div class="title">RAD Dropbox Uploader</div>
+<div class="sub">Version 1.1.3</div>
+</div>
---
+++ inc/sidebar.php
@@ -0,0 +1,26 @@
+<div class="meta-box-sortables">
+<div id="about" class="postbox">
+<h3 class="hndle" id="about-sidebar">Connect With Us!</h3>
+<div class="inside">
+<p>
+<a href="http://www.facebook.com/afIndustries" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/facebook.png "></a>&nbsp;
+<a href="http://www.twitter.com/intent/user?screen_name=afIndustries" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/twitter.png "></a>&nbsp;
+<a href="http://www.afcreative.ca/blog" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/wordpress.png "></a>&nbsp;
+<a href="http://www.afcreative.ca/feed" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/rss.png "></a>&nbsp;
+<a href="http://www.afcreative.ca/contact" target="_blank"><img src="<?php echo WP_PLUGIN_URL ?>/rad-dropbox-uploader/assets/email.png "></a>
+</p>
+</div>
+</div>
+</div>
+
+<div class="meta-box-sortables">
+<div id="about" class="postbox">
+<h3 class="hndle" id="about-sidebar">Resources & Information</h3>
+<div class="inside">
+<p>
+<li><a href="http://wordpress.org/plugins/rad-dropbox-uploader/" target="_blank">Plugin Homepage</a></li>
+<li><a href="http://www.afcreative.ca/" target="_blank">af Creative</a></li>
+</p>
+</div>
+</div>
+</div>
